c899225db0d33c077efc24c6e024cd285860e903
Srilekha.Talluri <30970454+Srilekha26@users.noreply.github.com>
2018-01-16 22:52:06 +0530

    Update 1-5.c

--- 1-5.c
+++ 1-5.c
@@ -3,8 +3,10 @@
 int main(){
 	int n;
 	scanf("%d",&n);
-	long long int k=pow(2,n);
-	long long int l=k%(long long int)pow(10,10);
-	printf("%lld",l);
+	if(n>=0&&n<=100){
+		long long int k=pow(2,n);
+		long long int l=k%(long long int)pow(10,10);
+		printf("%lld",l);
+	}
 	return 0;
 }
