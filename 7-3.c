17a0d1e6715ed3bad34cd983ce737e90c9a5470b
Srilekha.Talluri <30970454+Srilekha26@users.noreply.github.com>
2018-03-19 08:17:25 +0530

    Update 7-3.c

--- 7-3.c
+++ 7-3.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
  void merge(int *a,int fi,int mid,int la,int* b,int k){
-            int i=(fi-1)*k,j=(mid)*k,d=0;
-            while(i<(mid)*k&&j<la*k-1){
+            int i=(fi-1)*k,j=(mid-1)*k,d=(fi-1)*k;
+            while(i<(mid-1)*k&&j<la*k-1){ 
                 if(a[i]<a[j]) 
-                b[d++]=a[i++];
-                else b[d++]=a[j++];
+                  b[d++]=a[i++]; 
+                else  
+                  b[d++]=a[j++];
             }
-            while(i<(mid)*k)  b[d++]=a[i++];
-            while(j<(la*k-1)) b[d++]=a[j++];
+            
+            while(i<(mid-1)*k)
+            b[d++]=a[i++];
+            while(j<(la*k)) 
+                b[d++]=a[j++];
+            
  }
  void mergesort(int* a,int fi,int la,int* b,int k){
         int mid=(fi+la)/2;
-        while(mid!=fi){
+        if(mid!=fi){
             
             mergesort(a,fi,mid-1,b,k);
             mergesort(a,mid,la,b,k);
@@ -29,8 +34,8 @@ int main() {
     }
     int fi,la;
     fi=1;la=n;
-   mergesort(l,fi,la,b,k);
-   for(i=0;i<n*k;i++) printf("%d",b[i]);
+  mergesort(l,fi,la,b,k);
+   for(i=0;i<n*k;i++) printf("%d  ",b[i]);
    return 0;
         
     }
